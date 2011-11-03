13d8293481142b08cfa7a72e516a8725dbec5bb3
Niko Felger <niko.felger@gmail.com>
2011-11-03 17:11:38 +0000

    Generating 6x4 index cards

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,4 +1,5 @@
 require 'prawn'
+require "prawn/measurement_extensions"
 
 class CardGenerator
   def initialize(stories)
@@ -6,7 +7,10 @@ class CardGenerator
   end
 
   def generate
-    pdf = Prawn::Document.new
+    pdf = Prawn::Document.new(
+      :page_layout => :portrait,
+      :page_size   => [15.24.cm, 10.16.cm]
+    )
     @stories.each do |story|
       pdf.text story[:title]
       pdf.start_new_page
